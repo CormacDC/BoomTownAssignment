fbc118e764dfa75f75779e7a886a46a6a681e66a
CormacDC <60583567+CormacDC@users.noreply.github.com>
2022-01-28 12:16:04 -0500

    CrawlURLs() correctly iterates through links containing BoomTown github api link

--- BoomTownAPIService.cs
+++ BoomTownAPIService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using System;
+using System.Net.Http.Headers;
 using Newtonsoft.Json;
 
 class BoomTownAPIService
@@ -12,40 +13,66 @@ class BoomTownAPIService
         HttpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Product", "1.0"));
 	}
 
-	public async Task<object> GetBoomTownObject(string url)
+	public async Task<string> GetBoomTownObjectString(string url)
 	{
 		using (var request = new HttpRequestMessage(HttpMethod.Get, url))
 		{
 			var response = await HttpClient.SendAsync(request);
 
-			response.EnsureSuccessStatusCode();
+            int status = (int)response.StatusCode;
+            if (status != 200)
+            {
+                Console.WriteLine("Something went wrong; got response code " + status);
+                return string.Empty;
+            }
 
             string responseString = await response.Content.ReadAsStringAsync();
 
-            //dynamic returnObject = JsonSerializer.Deserialize<BoomTown>(responseString);
-            object returnObject = JsonConvert.DeserializeObject(responseString);
-
-			return returnObject != null ? returnObject : string.Empty;
+			return responseString != null ? responseString : string.Empty;
 		}
 	}
 
-    public void getIDs(string url){
-        
+    public void printIDs(string url)
+    {
+        string objString = GetBoomTownObjectString(url).Result;
+
+        if (objString == string.Empty)
+        {
+            Console.WriteLine("Something went wrong getting IDs from URL " + url + "\n");
+            return;
+        }
+
+        dynamic obj = JsonConvert.DeserializeObject(objString);
+
+        foreach (var group in obj)
+        {
+            //Console.WriteLine((string)group.id);
+            Console.WriteLine("iterates correctly");
+        }
     }
 
-    public void CrawlURLs(object json, string url){
+    public void CrawlURLs(string jsonString, string url)
+    {
+        Dictionary<string,string> json = new Dictionary<string, string>();
+        json = JsonConvert.DeserializeObject<Dictionary<string,string>>(jsonString);
 
+        foreach (var element in json)
+        {
+            if (element.Value != null && element.Value.Contains(TOPLEVELURL, comparisonType: StringComparison.OrdinalIgnoreCase))
+            {
+                printIDs(element.Value);
+            }
+        }
     }
     
     static void Main(string[] args)
     {
         BoomTownAPIService boomTown = new BoomTownAPIService();
-        object topLevel = boomTown.GetBoomTownObject(TOPLEVELURL).Result;
 
-        //string topLevelString = JsonSerializer.Serialize<BoomTown>(topLevel);
-        string topLevelString = JsonConvert.SerializeObject(topLevel);
-        Console.WriteLine("{0}",topLevelString);
+        string topLevelString = boomTown.GetBoomTownObjectString(TOPLEVELURL).Result;
+        //Console.WriteLine("{0}",topLevelString);
 
+        boomTown.CrawlURLs(topLevelString, TOPLEVELURL);
 
     }
 }
